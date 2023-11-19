2dcc97a505edfbdcfeb731cfd5a374b9a22ffe97
shashank084 <shashankgs420@gmail.com>
2023-11-19 22:40:38 +0530

    Begin

---
+++ app.js
@@ -0,0 +1,57 @@
+
+const dotenv = require("dotenv")
+const mongoose = require('mongoose')
+
+const express = require('express');
+const app = express();
+
+dotenv.config({path:'./config.env'});
+
+require('./db/conn');
+
+const User = require('./model/userSchema');
+
+//app.use(require('./router/auth'));
+
+//const PORT = process.env.PORT;
+
+const PORT = process.env.PORT || DEFAULT_PORT;
+
+//middleware
+
+const middlware = (req,res,next) => {
+    console.log('hello');
+    next();
+}
+
+app.get('/', (req, res) => {
+    res.send("Hello earth");
+});
+
+app.get('/pricing', (req, res) => {
+    res.send("pricing");
+});
+
+app.get('/about', middlware, (req, res) => {
+    res.send("about");
+});
+
+app.get('/services', (req, res) => {
+    res.send("services");
+});
+
+app.get('/contact', (req, res) => {
+    res.send("contact");
+});
+
+app.get('/login', (req, res) => {
+    res.send("login");
+});
+
+app.get('/signup', (req, res) => {
+    res.send("signup");
+});
+
+app.listen(PORT, () => {
+    console.log('server is running');
+})
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+
+    res.send("contact");
